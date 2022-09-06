d116aec6f4f440dc8ef8e8f7d08e6f6747d0c9bd
holmes <holmes@weareclario.com>
2022-09-06 21:56:12 +0200

    Write to db

--- high_load_4_siege_perfomance/backend/aspnetapp/Controllers/Controllers/WeatherForecastController.cs
+++ high_load_4_siege_perfomance/backend/aspnetapp/Controllers/Controllers/WeatherForecastController.cs
@@ -1,3 +1,6 @@
+using app.Services;
+using InfluxDB.Client.Api.Domain;
+using InfluxDB.Client.Writes;
 using Microsoft.AspNetCore.Mvc;
 
 namespace backend.Controllers;
@@ -11,15 +14,36 @@ public class WeatherForecastController : ControllerBase
         "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
     };
 
-    private readonly ILogger<WeatherForecastController> _logger;
+    private readonly InfluxDBService _influxDBService;
+    private readonly string _org = Environment.GetEnvironmentVariable("DOCKER_INFLUXDB_INIT_ORG") ?? "maxim";
+    private readonly string _bucket = Environment.GetEnvironmentVariable("DOCKER_INFLUXDB_INIT_BUCKET") ?? "weather";
 
-    public WeatherForecastController(ILogger<WeatherForecastController> logger)
+    public WeatherForecastController(InfluxDBService influxDBService)
     {
-        _logger = logger;
+        _influxDBService = influxDBService;
     }
 
     [HttpGet(Name = "GetWeatherForecast")]
     public IEnumerable<WeatherForecast> Get()
+    {
+        var weather = GetWeather();
+        _influxDBService.Write(write =>
+        {
+            foreach (var item in weather)
+            {
+                var point = PointData.Measurement("weather")
+                .Tag("date", item.Date.ToString())
+                .Tag("Summary", item.Summary)
+                .Field("TemperatureC", item.TemperatureC)
+                .Timestamp(DateTime.UtcNow, WritePrecision.Ns);
+
+                write.WritePoint(point, _bucket, _org);
+            }
+        });
+        return weather;
+    }
+
+    private static IEnumerable<WeatherForecast> GetWeather()
     {
         return Enumerable.Range(1, 5).Select(index => new WeatherForecast
         {
