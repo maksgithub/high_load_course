c7b029fb59b58cd0eff13b4f04708e42bc6286d1
holmes <holmes@weareclario.com>
2022-10-09 19:11:19 +0200

    9.1

--- high_load_9.1_redis/node/common.ts
+++ high_load_9.1_redis/node/common.ts
@@ -1,9 +1,12 @@
+import delay from "delay";
 import { createClient } from "redis";
 
 const client = createClient({
   url: "redis://localhost:6380",
   password: "str0ng_passw0rd",
 });
+
+type Record = { value: string, delta: number }
 
 export async function connect() {
   console.log("Start connecting...");
@@ -17,7 +20,7 @@ export async function connect() {
 
 export async function sendCommand<T>(
   cmd: string,
-  silent = false
+  silent = true
 ): Promise<T> {
   if (!silent) {
     console.log("\nStart executing command: ", cmd);
@@ -30,10 +33,53 @@ export async function sendCommand<T>(
   return result;
 }
 
+export async function printAllKeys(): Promise<void> {
+  const keys = await sendCommand<string[]>("KEYS *");
+  console.log({ keys });
+}
+
+export async function writeKey(key: string, ttl: number): Promise<string> {
+  const start = Date.now();
+  const value = await recomputeValue();
+  const delta = Date.now() - start;
+  await setKey(key, { value, delta }, ttl);
+  return await sendCommand<string>(`GET ${key}`);
+}
+
+export async function fetch(key: string, newTtl: number, beta = 1) {
+  const result = await sendCommand<string>(`GET ${key}`)
+  if (!result) {
+    return writeKey(key, newTtl);
+  }
+  const { value, delta } = JSON.parse(result);
+  const ttl = await sendCommand<number>(`TTL ${key}`);
+  const expire = Date.now() + ttl * 1000;
+  const res = Date.now() - delta * beta * Math.log(Math.random());
+  if (!value || res >= expire) {
+    return writeKey(key, newTtl);
+  }
+  return await sendCommand<string>(`GET ${key}`);
+}
+
 export function generateValue(len: number): string {
   let res = "";
   for (let i = 0; i < len; i++) {
     res += "A";
   }
   return res;
+}
+
+
+async function setKey<T>(key: string, value: T, ttl?: number): Promise<void> {
+  await sendCommand(`SET ${key} ${JSON.stringify(value)}`);
+  if (ttl) {
+    await sendCommand(`EXPIRE ${key} ${ttl}`);
+  }
+}
+
+let c = 0;
+async function recomputeValue(): Promise<string> {
+  c += 1;
+  await delay(1200);
+  return `value_${c}`;
 }
--- high_load_9.1_redis/node/run.ts
+++ high_load_9.1_redis/node/run.ts
@@ -1,6 +1,11 @@
-import { connect, generateValue, sendCommand } from "./common";
+import delay from "delay"
+import { connect, fetch, generateValue, sendCommand } from "./common";
 
-async function run() {
+function getTime() {
+  return new Date().toLocaleString(undefined, { hour: '2-digit', hour12: false, minute: '2-digit', second: '2-digit' });
+}
+
+async function tryEvictionStrategy() {
   await connect();
   await sendCommand("FLUSHALL");
   await sendCommand("config set maxmemory 1250kb");
@@ -16,16 +21,24 @@ async function run() {
     }
   }
 
-  for (let i = 0; i < 5; i++) {
-    try {
-    //   await sendCommand(`GET name${i}`);
-
-    } catch (error) {
-      break;
-    }
-  }
   const keys = await sendCommand<string[]>("KEYS *", true);
   console.log(keys.sort((n1, n2) => (n1 > n2 ? 1 : -1)));
+}
+
+async function run() {
+  await connect();
+  const key = 'key_3';
+  const ttl = 60;
+  await fetch(key, ttl);
+
+
+  while (true) {
+    await delay(2000);
+    const res = await fetch(key, ttl);
+    const val = JSON.parse(res);
+    const time = getTime();
+    console.log(time, val.value);
+  }
 }
 
 run().catch(console.error);
