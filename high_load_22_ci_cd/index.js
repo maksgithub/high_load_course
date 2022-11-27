8e9feeaa585461645e86041ee301b96636f90ff4
max <maxim.tatsenko@gmail.com>
2022-11-27 19:14:17 +0100

    Add dotnet

--- high_load_22_ci_cd/index.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const app = express();
-const port = 8080;
-
-
-app.get('/status', (req, res) => res.send({status: "I'm up and running"}));
-app.listen(port, () => console.log(`Dockerized Nodejs Applications is listening on port ${port}!`));
