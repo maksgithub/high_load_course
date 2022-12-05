706f2bcc4ce3e20da97fae465e34d81192abd2d9
max <maxim.tatsenko@gmail.com>
2022-12-05 21:01:15 +0100

    Add integration test

--- high_load_22_ci_cd/node/integration/integration.spec.js
+++ high_load_22_ci_cd/node/integration/integration.spec.js
@@ -1,9 +1,8 @@
-describe('test', () => {
-    it('test_1', () => {
-        expect(true).toEqual(true)
-    })
+const axios = require('axios');
 
-    it('test2', () => {
-        expect(true).toEqual(true)
+describe('test', () => {
+    it('test_1', async () => {
+        const res = await axios.get("http://ec2-100-25-129-91.compute-1.amazonaws.com/")
+        expect(res.data).toEqual("Hello World_5!!")
     })
 })
