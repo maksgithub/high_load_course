792b5674ac845e8159e782a1c258ee0224e446a7
max <maxim.tatsenko@gmail.com>
2022-12-05 22:56:17 +0100

    _8

--- high_load_22_ci_cd/node/index.js
+++ high_load_22_ci_cd/node/index.js
@@ -3,7 +3,7 @@ const app = express()
 const port = process.env.SERVER_PORT
 
 app.get('/', (req, res) => {
-  res.send('Hello World_7!!')
+  res.send('Hello World_8!!')
 })
 
 app.get('/test', (req, res) => {
